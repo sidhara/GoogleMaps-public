c523b2cd569749c8c7948b53b2ea1b264797ee0a
Sidhar Araujo <47336010+sidhara@users.noreply.github.com>
2021-04-15 16:21:07 -0500

    clean

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:googlemapsflutter/screens/MapsDemo.dart';
 
-/*import para ejecutar en segundo plano */
-import 'package:workmanager/workmanager.dart';
-
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Workmanager.initialize(callbackDispatcher);
-  await Workmanager.registerPeriodicTask("test_workertask", "test_workertask",
-      inputData: {"data1": "value1", "data2": "value2"},
-      frequency: Duration(minutes: 10));
+void main(){
   runApp(MapsDemo());
 }
